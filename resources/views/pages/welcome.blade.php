76adbfd3f27007f1df5c8abb63742cde1ecfcec8
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-03-15 00:10:36 +0100

    exercise completed

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.App')
 @section('title', 'Pagina di Benvenuto')
+
+
+
 @section('main')
 
-<h1 class="text-warning">ciao</h1>
+<main class="container">
+    <div class="row d-flex justify-content-center pt-5 gap-3">
+        @foreach ($movies as $item)
+
+            <div id="card" class="card col-2">
+                <div class=" d-flex flex-column justify-content-center card-body">
+                <h5 class="card-title">{{ $item['title'] }}</h5>
+                <p class="card-text">{{$item['original_title']}}</p>
+                <p class="card-text">{{$item['nationality']}}</p>
+                </div>
+            </div>
+
+        @endforeach
+    </div>
+
+</main>
 
 @endsection
 
